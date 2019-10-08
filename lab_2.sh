ab87c938ac742242425b8303ef00a3553eaef52b
VceDovolnoProsto <55354101+VceDovolnoProsto@users.noreply.github.com>
2019-10-08 23:30:23 +0300

    Add files via upload

---
+++ lab_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Следуем инструкции установки с сайта https://mavlink.io/en/getting_started/installation.html
+
+rm -rf mavlink                                                   # Рекурсивно удаляем без подтверждения и кода ошибочного заверщения каталок mavlink
+sudo apt-get install python3-pip                                 # Загружаем Python 2.7+ or 3.3+
+pip install --user future                                        # Загружаем the future module
+sudo apt-get install python-tk                                   # Загружаем TkInter
+git clone https://github.com/mavlink/mavlink.git --recursive     # Клонируем the mavlink repo в доступный для записи каталог
+PYTHONPATH=$PWD/mavlink                                          # Устанавливаем путь к каталогу загрузки
